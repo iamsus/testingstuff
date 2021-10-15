62d29838abe3fce992e2bb52ab220692a9c3798b
iamsus <80224968+iamsus@users.noreply.github.com>
2021-10-15 18:36:28 -0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,14 @@
-import time
-print("Hello World!")
-time.sleep(1000)
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello World</hi>"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
